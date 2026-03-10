fb8e5d3693a80142fe8a0c8a1b0147d4e37ccaea
Ungureanu Stefan <stefannungureanuu@gmail.com>
2026-03-10 19:54:36 +0200

    lab-02: find-max.c: Implemented find-max.
    Signed-off-by: Ungureanu Stefan <stefannungureanuu@gmail.com>

--- labs/lab-02/tasks/find_max/support/find_max.c
+++ labs/lab-02/tasks/find_max/support/find_max.c
@@ -1,35 +1,24 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "find_max.h"
 
 void *find_max(void *arr, int n, size_t element_size,
-				int (*compare)(const void *, const void *))
+               int (*compare)(const void *, const void *))
 {
-	void *max_elem = arr;
+    void *max_elem = arr;
 
-	(void) n;
-	(void) element_size;
-	(void) compare;
+    char *p;
+    for (p = arr; p - (char *)arr < n; p += element_size) {
+        if (compare(p, max_elem) >= 0) {
+            max_elem = p;
+        }
+    }
 
-	/**
-	 * TODO: Implement function
-	 */
-
-	return max_elem;
+    return max_elem;
 }
 
-int compare(const void *a, const void *b)
-{
-	(void) a;
-	(void) b;
-
-	/**
-	 * TODO: Implement function
-	 */
-
-	return 0x69;
-}
+int compare(const void *a, const void *b) { return *(int *)a - *(int *)b; }
