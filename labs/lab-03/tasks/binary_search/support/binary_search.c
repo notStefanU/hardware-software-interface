bb5fd3d170b0c30c0b4d7caab05cec0f54acf613
Ungureanu Stefan <stefannungureanuu@gmail.com>
2026-03-17 19:24:42 +0200

    lab-03: binary_search & vector_max: Implemented desired functions.
    Signed-off-by: Ungureanu Stefan <stefannungureanuu@gmail.com>

--- labs/lab-03/tasks/binary_search/support/binary_search.c
+++ labs/lab-03/tasks/binary_search/support/binary_search.c
@@ -6,12 +6,32 @@
 
 int binary_search(int *v, int len, int dest)
 {
-	int start = 0;
-	int end = len - 1;
-	int middle;
+    int start = 0;
+    int end = len - 1;
+    int middle;
 
-	/**
-	 * TODO: Implement binary search
-	 */
-	return -1;
+my_while:
+    if (start > end) {
+        goto not_found;
+    }
+    middle = (start + end) / 2;
+    if (v[middle] == dest) {
+        goto found;
+    }
+
+    if (v[middle] < dest) {
+        start = middle + 1;
+        goto my_while;
+    }
+
+    if (v[middle] > dest) {
+        end = middle - 1;
+        goto my_while;
+    }
+
+not_found:
+    return -1;
+
+found:
+    return middle;
 }
--- labs/lab-03/tasks/vector_max/support/vector_max.c
+++ labs/lab-03/tasks/vector_max/support/vector_max.c
@@ -6,11 +6,22 @@
 
 int vector_max(int *v, int len)
 {
-	int max;
-	unsigned int i;
+    int max;
+    unsigned int i;
 
-	/**
-	 * TODO: Implement finding the maximum value in the vector
-	 */
-	return -1;
+    i = 0;
+    max = v[0];
+
+my_loop:
+    if (max < v[i]) {
+        max = v[i];
+    }
+    i++;
+
+    if (i < len) {
+        goto my_loop;
+    }
+    return max;
+
+    return -1;
 }
