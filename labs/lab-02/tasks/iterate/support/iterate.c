875273deb1d435720f80ed1f80a5db88f9a38d75
Ungureanu Stefan <stefannungureanuu@gmail.com>
2026-03-17 11:31:31 +0200

    lab-02: pixels.c: Implemented reverse_pic and color_to_gray functions.
    Signed-off-by: Ungureanu Stefan <stefannungureanuu@gmail.com>

--- labs/lab-02/tasks/iterate/support/iterate.c
+++ labs/lab-02/tasks/iterate/support/iterate.c
@@ -2,45 +2,45 @@
 
 #include <stdio.h>
 
-#include "iterate.h"
 #include "array.h"
+#include "iterate.h"
 
 void print_chars(void)
 {
-	/**
-	 * TODO: Implement function
-	 */
-	(void) v;
+    /**
+     * TODO: Implement function
+     */
+    (void)v;
 
-	printf("-------------------------------\n");
+    printf("-------------------------------\n");
 }
 
 void print_shorts(void)
 {
-	/**
-	 * TODO: Implement function
-	 */
-	(void) v;
+    /**
+     * TODO: Implement function
+     */
+    (void)v;
 
-	printf("-------------------------------\n");
+    printf("-------------------------------\n");
 }
 
 void print_ints(void)
 {
-	/**
-	 * TODO: Implement function
-	 */
-	(void) v;
+    /**
+     * TODO: Implement function
+     */
+    (void)v;
 
-	printf("-------------------------------\n");
+    printf("-------------------------------\n");
 }
 
 void print_long_longs(void)
 {
-	/**
-	 * TODO: Implement function
-	 */
-	(void) v;
+    /**
+     * TODO: Implement function
+     */
+    (void)v;
 
-	printf("-------------------------------\n");
+    printf("-------------------------------\n");
 }
--- labs/lab-02/tasks/pixels/support/pixels.c
+++ labs/lab-02/tasks/pixels/support/pixels.c
@@ -1,20 +1,61 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <time.h>
-
-#include "pixel.h"
 #include "pixels.h"
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "pixel.h"
+
+#define GET_PIXEL(a, i, j) (*(*((a) + (i)) + (j)))
+#define DIE(assertion, call_description)                                       \
+    do {                                                                       \
+        if (assertion) {                                                       \
+            fprintf(stderr, "(%s, %d): ", __FILE__, __LINE__);                 \
+            perror(call_description);                                          \
+            exit(errno);                                                       \
+        }                                                                      \
+    } while (0)
+
 
 void reverse_pic(struct picture *pic)
 {
-	(void) pic;
+    if (pic == NULL || pic->pix_array == NULL) {
+        puts("Error: pic/pix_array uninitialized!");
+        return;
+    }
+
+    int i, j;
+    for (i = 0; i < pic->height / 2; i++) {
+        for (j = 0; j < pic->width; j++) {
+            struct pixel *up = &GET_PIXEL(pic->pix_array, i, j);
+            struct pixel *down =
+                &GET_PIXEL(pic->pix_array, pic->height - 1 - i, j);
+
+            struct pixel temp = *up;
+            *up = *down;
+            *down = temp;
+        }
+    }
 }
 
 void color_to_gray(struct picture *pic)
 {
-	(void) pic;
+    if (pic == NULL || pic->pix_array == NULL) {
+        puts("Error: pic/pix_array uninitialized!");
+        return;
+    }
+
+    int i, j;
+    for (i = 0; i < pic->height; i++) {
+        for (j = 0; j < pic->width; j++) {
+            struct pixel *pix = &GET_PIXEL(pic->pix_array, i, j);
+
+            pix->R = 0.3 * pix->R;
+            pix->G = 0.59 * pix->G;
+            pix->B = 0.1 * pix->B;
+        }
+    }
 }
