b4dd98206e80f55d71d89a2b966de3f41d12b08d
Ungureanu Stefan <stefannungureanuu@gmail.com>
2026-04-21 18:36:07 +0300

    lab-07: max.asm: Implemented max
    Signed-off-by: Ungureanu Stefan <stefannungureanuu@gmail.com>

--- labs/lab-07/tasks/max/support/max.asm
+++ labs/lab-07/tasks/max/support/max.asm
@@ -12,8 +12,17 @@ main:
     mov rax, 1
     mov rbx, 4
 
-    ; TODO: get maximum value. You are only allowed to use one conditional jump and push/pop instructions.
+    push rax
+    push rbx
 
+    cmp rax, rbx
+    ja greater
+    pop rax
+    pop rbx
+
+    ; TODO: get maximum value. You are only allowed to use one conditional jump and push/pop instructions.
+    
+greater:
     PRINTF64 `Max value is: %ld\n\x0`, rax ; print maximum value
 
     mov rsp, rbp
