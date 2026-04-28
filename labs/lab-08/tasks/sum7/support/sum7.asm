3332221b2e9efe313de85b6fe14e4f383a28e244
Ungureanu Stefan <stefannungureanuu@gmail.com>
2026-04-28 18:42:26 +0300

    Lab-08: sum7.asm: Implement sum
    Signed-off-by: Ungureanu Stefan <stefannungureanuu@gmail.com>

--- labs/lab-08/tasks/sum7/support/sum7.asm
+++ labs/lab-08/tasks/sum7/support/sum7.asm
@@ -10,6 +10,18 @@ sum7:
 
     ; TODO: implement the sum7 function
 
+    xor rax, rax
+
+    mov rax, rdi
+    add rax, rsi
+    add rax, rdx
+    add rax, rcx
+    add rax, r8
+    add rax, r9
+    add rax, [rbp + 16]
+
+
+
     ; TODO: restore the used registers and the stack pointer, if altered
 
     leave
