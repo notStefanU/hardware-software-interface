f422e95e4e5caaf7003cd8a2236e0c5c2383d952
Ungureanu Stefan <stefannungureanuu@gmail.com>
2026-05-19 19:45:38 +0300

    lab-11: obj-link-dev: hackerman
    Signed-off-by: Ungureanu Stefan <stefannungureanuu@gmail.com>

--- labs/lab-11/tasks/obj-link-dev/support/main.c
+++ labs/lab-11/tasks/obj-link-dev/support/main.c
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
+extern int qty;
 int main(void)
 {
-	/*
-	 * TODO: Make it so you print:
-	 *    price is 21
-	 *    quantity is 42
-	 * without directly calling a printing function.
-	 */
+    set_price(21);
+    print_price();
+    qty = 42;
+    print_quantity();
+    /*
+     * TODO: Make it so you print:
+     *    price is 21
+     *    quantity is 42
+     * without directly calling a printing function.
+     */
 
-	return 0;
+    return 0;
 }
